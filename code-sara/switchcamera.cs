a08f4cc42da6858e130abef0fa83865d2850b581
TzunderWulf <69017007+TzunderWulf@users.noreply.github.com>
2023-06-28 22:05:18 +0200

    Update switchcamera.cs

--- code-sara/switchcamera.cs
+++ code-sara/switchcamera.cs
@@ -1,1 +1,35 @@
-// interesting document about switching camera's, soon to come
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerTracker : MonoBehaviour
+{
+    public Transform player;
+    public float maxDistance = 10;
+    public float moveSpeed = 20;
+    public float updateSpeed = 10;
+    [Range(0, 10)]
+    public float currentDistance = 5;
+    private string moveAxis = "Mouse ScrollWheel";
+    private GameObject ahead;
+    private MeshRenderer _renderer;
+    public float hideDistance = 1.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ahead = new GameObject("ahead");
+        _renderer = player.gameObject.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        ahead.transform.position = player.position + player.forward * (maxDistance * 0.25f);
+        currentDistance += Input.GetAxisRaw(moveAxis) * moveSpeed * Time.deltaTime;
+        currentDistance = Mathf.Clamp(currentDistance, 0, maxDistance);
+        transform.position = Vector3.MoveTowards(transform.position, player.position + Vector3.up * currentDistance - player.forward * (currentDistance + maxDistance * 0.5f), updateSpeed * Time.deltaTime);
+        transform.LookAt(ahead.transform);
+        _renderer.enabled = (currentDistance > hideDistance);
+    }
+}
